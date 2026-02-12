255e7cc46387e3ab4ae7b25d73139ec0c3151e68
ya947045-lang <ya947045@gmail.com>
2026-02-12 12:58:06 +0200

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+export const ORDER_STATUS = {
+  PENDING: 'pending',
+  CONFIRMED: 'confirmed',
+  SHIPPED: 'shipped',
+  DELIVERED: 'delivered'
+};
+
+export const USER_ROLES = {
+  ADMIN: 'admin',
+  CUSTOMER: 'customer'
+};
+
+export const PRODUCT_CATEGORIES = [
+  'Smartphones',
+  'Laptops',
+  'Tablets',
+  'Accessories',
+  'Audio',
+  'Gaming'
+];
+
+export const SHIPPING_COST = 0;
+export const TAX_RATE = 0.1;
